2de678d4d6197a0e0fc1795938ffdeafcb7c02e4
josegx10 <jborquezsandoval3@gmail.com>
2023-09-25 09:19:49 -0700

    Configuracion de inicio de spinner y sin resultado

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -11,55 +11,55 @@ const PeopleList = ({ people, loading }) => {
       {loading ? (
         <div class="spinner"></div>
       ) : (
-        <table class="default">
-          <tr>
-            <th>Nombre</th>
+        <><>{people.length === 0 ? (<h2> Sin resultados </h2>) : 'nada'}</><table class="default">
+            <tr>
+              <th>Nombre</th>
 
-            <th>Altura</th>
+              <th>Altura</th>
 
-            <th>Peso</th>
+              <th>Peso</th>
 
-            <th>Color de cabello</th>
+              <th>Color de cabello</th>
 
-            <th>Color de piel</th>
+              <th>Color de piel</th>
 
-            <th>Color de ojos</th>
+              <th>Color de ojos</th>
 
-            <th>Fecha de nacimiento</th>
+              <th>Fecha de nacimiento</th>
 
-            <th>Género</th>
-          </tr>
-          {people &&
-            people?.map((item) => (
-              <>
-                <tr
-                  onClick={() => {
-                    setIsOpen(true);
-                    setItem(item);
-                  }}
-                >
-                  <td>{item?.name === 'none' || item?.name === 'unknown' ? ( 'n/a' ): item?.name}</td>
+              <th>Género</th>
+            </tr>
+            {people &&
+              people?.map((item) => (
+                <>
+                  <tr
+                    onClick={() => {
+                      setIsOpen(true);
+                      setItem(item);
+                    } }
+                  >
+                    <td>{item?.name === 'none' || item?.name === 'unknown' ? ('n/a') : item?.name}</td>
 
-                  <td>{item?.height === 'none' ||item?.height  === 'unknown' ? ('n/a'): item?.height }</td>
+                    <td>{item?.height === 'none' || item?.height === 'unknown' ? ('n/a') : item?.height}</td>
 
-                  <td>{item?.mass === 'none' ||item?.mass  === 'unknown' ? ('n/a'): item?.mass}</td>
+                    <td>{item?.mass === 'none' || item?.mass === 'unknown' ? ('n/a') : item?.mass}</td>
 
-                  <td>{item?.hair_color === 'none' ||item?.hair_color  === 'unknown' ? ('n/a'): item?.hair_color} </td>
+                    <td>{item?.hair_color === 'none' || item?.hair_color === 'unknown' ? ('n/a') : item?.hair_color} </td>
 
-                  <td>{item?.skin_color === 'none' ||item?.skin_color  === 'unknown' ? ('n/a'): item?.skin_color}</td>
+                    <td>{item?.skin_color === 'none' || item?.skin_color === 'unknown' ? ('n/a') : item?.skin_color}</td>
 
-                  <td>{item?.eye_color === 'none' ||item?.eye_color  === 'unknown' ? ('n/a'): item?.eye_color}</td>
+                    <td>{item?.eye_color === 'none' || item?.eye_color === 'unknown' ? ('n/a') : item?.eye_color}</td>
 
-                  <td>{item?.birth_year === 'none' ||item?.birth_year  === 'unknown' ? ('n/a'): item?.birth_year}</td>
+                    <td>{item?.birth_year === 'none' || item?.birth_year === 'unknown' ? ('n/a') : item?.birth_year}</td>
 
-                  <td>{item?.gender === 'none' ||item?.gender  === 'unknown' ? ('n/a'): item?.gender}</td>
-                </tr>
-              </>
-            ))}
-            
-        </table>
+                    <td>{item?.gender === 'none' || item?.gender === 'unknown' ? ('n/a') : item?.gender}</td>
+                  </tr>
+                </>
+              ))}
+
+          </table></>
       )}
-      {people.length === 0? (<h2> Sin resultados </h2>) : 'nada'}
+      
       <div>
         {isOpen && (
           <Modal
